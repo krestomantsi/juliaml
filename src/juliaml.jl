ae79caf7933045a44b8dfaecaa36723ffa5834cc
nick <nlampri@gmail.com>
2023-08-02 02:10:51 +0300

    corrections uploaded

--- src/juliaml.jl
+++ src/juliaml.jl
@@ -27,19 +27,45 @@ function mygem(A::Matrix{Float32}, B::Matrix{Float32})::Matrix{Float32}
     return C
 end
 
+function relu(x::Matrix{Float32})::Matrix{Float32}
+    max.(x, 0.0f0)
+end
+
+function relu_prime(x::Matrix{Float32})::Matrix{Float32}
+    x .> 0.0f0
+end
+
+function none_activation(x::Matrix{Float32})::Matrix{Float32}
+    x
+end
+
+function none_activation_prime(x::Matrix{Float32})::Matrix{Float32}
+    ones(eltype(x), size(x))
+end
+
+function swish_scalar(x::Float32)::Float32
+    @fastmath x / (1.0f0 + exp(-x))
+end
+
+function swish(x::Matrix{Float32})::Matrix{Float32}
+    @. map(swish_scalar, x)
+end
+
+function swish_prime(x::Matrix{Float32})::Matrix{Float32}
+    @fastmath swish(x) .+ (1.0f0 .- swish(x)) .* exp.(-x) ./ (1.0f0 .+ exp.(-x))
+end
 
 # each layer will have a struct definition
 # a custom pullback function
 # and ofc a forward call
 # ideally i wanted static arrays here but the compilation takes for ever
+
 struct Dense
     weights::Matrix{Float32}
     bias::Matrix{Float32}
-    activation::Function
-    activation_prime::Function
+    activation::Union{typeof(relu),typeof(swish),typeof(none_activation)}
+    activation_prime::Union{typeof(relu_prime),typeof(swish_prime),typeof(none_activation_prime)}
 end
-
-
 # forward call of Dense
 function (d::Dense)(x::Matrix{Float32})::Matrix{Float32}
     output = mygem(d.weights, x) .+ d.bias
@@ -58,6 +84,7 @@ end
 
 struct MLP
     layers::Tuple{Vararg{Dense}}
+    # layers::Tuple{Dense,Dense,Dense}
 end
 
 function (mlp::MLP)(x::Matrix{Float32})::Matrix{Float32}
@@ -80,33 +107,6 @@ struct MLPGradient
     layers::Tuple{Vararg{DenseGradient}}
 end
 
-function relu(x::Matrix{Float32})::Matrix{Float32}
-    max.(x, 0.0f0)
-end
-
-function relu_prime(x::Matrix{Float32})::Matrix{Float32}
-    x .> 0.0f0
-end
-
-function swish_scalar(x::Float32)::Float32
-    @fastmath x / (1.0f0 + exp(-x))
-end
-
-function swish(x::Matrix{Float32})::Matrix{Float32}
-    swish_scalar.(x)
-end
-
-function swish_prime(x::Matrix{Float32})::Matrix{Float32}
-    @fastmath swish(x) .+ (1.0f0 .- swish(x)) .* exp.(-x) ./ (1.0f0 .+ exp.(-x))
-end
-
-function none_activation(x::Matrix{Float32})::Matrix{Float32}
-    x
-end
-
-function none_activation_prime(x::Matrix{Float32})::Matrix{Float32}
-    ones(eltype(x), size(x))
-end
 
 function mse(x::Matrix{Float32}, y::Matrix{Float32})::Float32
     sum((x .- y) .^ 2) / (size(x, 2) |> Float32)
@@ -118,11 +118,11 @@ end
 
 
 function MLP(input_size::Int, hidden_size::Int, output_size::Int, activation::Function, activation_prime::Function)
-    weights1 = randn(Float32, hidden_size, input_size) ./ sqrt(hidden_size)
+    weights1 = (randn(Float32, hidden_size, input_size) ./ (sqrt(hidden_size) |> Float32))
     bias1 = zeros(Float32, hidden_size, 1)
-    weights2 = randn(Float32, hidden_size, hidden_size) ./ sqrt(hidden_size)
+    weights2 = (randn(Float32, hidden_size, hidden_size) ./ (sqrt(hidden_size) |> Float32))
     bias2 = zeros(Float32, hidden_size, 1)
-    weights3 = randn(Float32, output_size, hidden_size) ./ sqrt(hidden_size)
+    weights3 = (randn(Float32, output_size, hidden_size) ./ (sqrt(hidden_size) |> Float32))
     bias3 = zeros(Float32, output_size, 1)
     layers = (
         Dense(weights1, bias1, activation, activation_prime),
@@ -131,18 +131,19 @@ function MLP(input_size::Int, hidden_size::Int, output_size::Int, activation::Fu
     MLP(layers)
 end
 
-model = MLP(1, 32, 1, swish, swish_prime)
 
-x = randn(Float32, 1, 1000)
-y = sin.(x)
+model = MLP(2, 32, 1, swish, swish_prime)
+
+x = randn(Float32, 2, 1000)
 y2 = model(x)
 
 display(@benchmark y2 = model(x))
 
 @report_opt model(x)
 @report_opt model.layers[1](x)
+# @code_warntype model.layers[1](x)
 
-activation = swish
+activation = relu
 weights = model.layers[1].weights
 bias = model.layers[1].bias
 # dense(weights, x, bias, activation)
